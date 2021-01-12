f0f939f776222b6a920d768b2229ff4be8f466dd
Rūdolfs Ošiņš <rudolfs@osins.org>
2021-01-12 12:49:35 +0100

    WIP connecting two nodes works
    Signed-off-by: Rūdolfs Ošiņš <rudolfs@osins.org>

--- cypress/integration/p2p.spec.ts
+++ cypress/integration/p2p.spec.ts
@@ -3,7 +3,7 @@ import * as commands from "../support/commands";
 context("p2p", () => {
   it.only("does stuff", () => {
     cy.log("stuff happens");
-    cy.task("killall");
+    cy.task("node kill all");
 
     cy.task("node start", 17246);
     cy.task("node onboard", 17246);
@@ -11,40 +11,28 @@ context("p2p", () => {
     cy.task("node start", 18000);
     cy.task("node onboard", 18000);
 
+    cy.task("node connect", [17246, 18000]);
+
     cy.task("node get", 17246).then(node => {
       cy.log(node.authToken);
       cy.setCookie("auth-token", node.authToken);
 
       cy.visit("./public/index.html?backend=localhost:17246");
+      commands.pick("settings").click();
     });
-    commands.pick("settings").click();
 
-    cy.wait(4000);
+    cy.wait(5000);
+
     cy.task("node get", 18000).then(node => {
       cy.log(node.authToken);
       cy.setCookie("auth-token", node.authToken);
 
       cy.visit("./public/index.html?backend=localhost:18000");
+      commands.pick("settings").click();
     });
-    commands.pick("settings").click();
-
-    cy.wait(4000);
-    cy.task("node get", 17246).then(node => {
-      cy.log(node.authToken);
-      cy.setCookie("auth-token", node.authToken);
-
-      cy.visit("./public/index.html?backend=localhost:17246");
-    });
-    commands.pick("settings").click();
 
     cy.wait(50000);
-    cy.task("killall");
+    cy.task("node kill all");
     cy.log("finished");
-  });
-
-  it("other stuff", () => {
-    commands.resetProxyState();
-    commands.onboardUser();
-    cy.visit("./public/index.html");
   });
 });
--- cypress/plugins/index.ts
+++ cypress/plugins/index.ts
@@ -1,12 +1,13 @@
-import { spawn } from "child_process";
 import axios from "axios";
 import * as path from "path";
+import * as childProcess from "child_process";
+import * as util from "util";
 import * as waitOn from "wait-on";
-const util = require("util");
-const exec = util.promisify(require("child_process").exec);
 
-let nodes: UpstreamNode[] = [];
-let host = "127.0.0.1";
+const exec = util.promisify(childProcess.exec);
+
+const nodes: UpstreamNode[] = [];
+const HOST = "127.0.0.1";
 
 const getNode = (id: NodeId) => {
   return nodes.find(node => {
@@ -14,12 +15,24 @@ const getNode = (id: NodeId) => {
   });
 };
 
+const sleep = async (ms: number) => {
+  await new Promise(r => setTimeout(r, ms));
+};
+
+const cookieHeadersFromToken = (token: string) => {
+  return {
+    headers: {
+      Cookie: [`auth-token=${token}; Path=/`],
+    },
+  };
+};
+
 type NodeId = number;
 
 interface UpstreamNode {
   id: NodeId;
-  httpListen: string;
-  peerListen: string;
+  httpListen: number;
+  peerListen: number;
   pid?: number;
   authToken?: string;
   peerAddress?: string;
@@ -27,29 +40,30 @@ interface UpstreamNode {
 
 export default (on, config) => {
   on("task", {
-    async killall() {
+    async "node kill all"() {
       try {
-        console.log("killall");
+        console.log("  ### node kill all");
         await exec("killall radicle-proxy");
       } catch {}
       return null;
     },
-    async "node start"(id) {
-      let node: UpstreamNode = { id: id, httpListen: id, peerListen: id };
+    async "node start"(id: NodeId) {
+      console.log(`  ### node start ${id}`);
 
-      console.log(`### node start ${id}`);
+      const node: UpstreamNode = { id: id, httpListen: id, peerListen: id };
+
       const pathToProxy = path.join(
         __dirname,
-        "../../proxy/target/release/radicle-proxy"
+        "../../proxy/target/debug/radicle-proxy"
       );
-      const process = spawn(
+      const process = childProcess.spawn(
         pathToProxy,
         [
           "--test",
           "--http-listen",
-          `${host}:${node.httpListen}`,
+          `${HOST}:${node.httpListen}`,
           "--peer-listen",
-          `${host}:${node.peerListen}`,
+          `${HOST}:${node.peerListen}`,
         ],
         {}
       );
@@ -59,70 +73,97 @@ export default (on, config) => {
       nodes.push(node);
 
       process.on("exit", () => {
-        console.log(`#[${id}] node terminated`);
+        console.log(`    #[${id}] node terminated`);
         // nodes.filter((x) => { return x != node.id })
         return null;
       });
 
       process.stderr.setEncoding("utf8");
       process.stderr.on("data", data => {
-        console.log(`  [${id}] STDERR: `, data);
+        console.log(`    [${id}] STDERR: ${data.trim()}`);
       });
+
       process.stdout.setEncoding("utf8");
       process.stdout.on("data", data => {
-        console.log(`  [#${id}] STDOUT: `, data);
+        console.log(`    [${id}] STDOUT: ${data.trim()}`);
       });
 
-      var opts = {
-        resources: [`tcp:${host}:${id}`],
-      };
-
       try {
-        await waitOn(opts);
+        await waitOn({ resources: [`tcp:${HOST}:${id}`] });
         // once here, all resources are available
-        console.log(`[#${id}] node started successfully`);
+        console.log(`    [${id}] node started successfully`);
         return null;
       } catch (err) {
-        console.log(`#[${id}] node didn't start up in a timely fashion`);
+        console.log(
+          `    ERROR: [${id}] node didn't start up in a timely fashion`
+        );
         process.kill();
         return null;
       }
     },
-    async "node onboard"(id) {
-      console.log(`### node onboard ${id}`);
+    async "node onboard"(id: NodeId) {
+      console.log(`  ### node onboard ${id}`);
 
-      let node = getNode(id);
+      const node = getNode(id);
 
       try {
-        const resp = await axios.post(`http://${host}:${id}/v1/keystore`, {
+        const resp = await axios.post(`http://${HOST}:${id}/v1/keystore`, {
           passphrase: "radicle-upstream",
         });
-        console.log(resp.statusText);
 
-        await new Promise(r => setTimeout(r, 100));
-        node.authToken = resp.headers["set-cookie"][0]
-          .split(";")[0]
-          .split("=")[1];
+        // FIXME: how can we avoid this sleep between the requests?
+        await sleep(500);
+
+        node.authToken = resp.headers["set-cookie"][0].match(
+          /auth-token=(.*);/
+        )[1];
 
         const resp1 = await axios.post(
-          `http://${host}:${id}/v1/identities`,
+          `http://${HOST}:${id}/v1/identities`,
           {
             handle: "secretariat",
           },
-          {
-            headers: {
-              Cookie: resp.headers["set-cookie"],
-            },
-          }
+          cookieHeadersFromToken(node.authToken)
         );
-        node.peerAddress = `${resp1.data.peerId}@${host}:${node.peerListen}`;
-        console.log("NODE: ", node);
+        node.peerAddress = `${resp1.data.peerId}@${HOST}:${node.peerListen}`;
       } catch (err) {
-        console.log(err);
+        console.log("    ERROR: Onboarding failure: ${err}", err);
       }
       return null;
     },
-    async "node get"(id) {
+    async "node connect"(nodeIds: NodeId[]) {
+      console.log(`  ### node connect ${nodeIds}`);
+      if (nodeIds.length < 2) {
+        console.log("    Supply at least 2 node IDs");
+        return null;
+      }
+
+      nodeIds.forEach(async nodeId => {
+        const thisNode = getNode(nodeId);
+        const remainingNodes = nodes.filter(node => {
+          return thisNode.id !== node.id;
+        });
+
+        try {
+          const resp = await axios.post(
+            `http://${HOST}:${nodeId}/v1/session/settings`,
+            {
+              appearance: { theme: "dark", hints: { showRemoteHelper: true } },
+              coco: {
+                seeds: remainingNodes.map(node => node.peerAddress),
+              },
+            },
+            cookieHeadersFromToken(thisNode.authToken)
+          );
+        } catch (err) {
+          console.log("    ERROR: ", err);
+        }
+
+        await sleep(500);
+      });
+      return null;
+    },
+    async "node get"(id: NodeId) {
       return getNode(id);
     },
   });
